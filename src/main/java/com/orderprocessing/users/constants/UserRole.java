9a2c19319fdd1d0ffe4efd2db5b5335e18ff1296
petruacsinte <petruacsinte@hotmail.com>
2026-03-25 06:45:22 -0400

    Fixing user role constants.

--- src/main/java/com/orderprocessing/users/constants/UserRole.java
+++ src/main/java/com/orderprocessing/users/constants/UserRole.java
@@ -4,9 +4,9 @@ public class UserRole {
 
 		private UserRole() {}
 
-		public static final String ADMIN = "ADMIN"; //$NON-NLS-1$
+		public static final String ADMIN = "ROLE_ADMIN"; //$NON-NLS-1$
 		public static final short ADMIN_ID = 0;
-	    public static final String USER = "USER"; //$NON-NLS-1$
+	    public static final String USER = "ROLE_USER"; //$NON-NLS-1$
 	    public static final short USER_ID = 1;
 
 }
