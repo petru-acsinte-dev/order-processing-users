5e65599e706187d41a60dfcd22927463cd28b66d
petruacsinte <petruacsinte@hotmail.com>
2026-04-07 07:39:33 -0400

    Fixing context path check for authentication.

--- src/main/java/com/orderprocessing/users/security/JsonLoginFilter.java
+++ src/main/java/com/orderprocessing/users/security/JsonLoginFilter.java
@@ -47,7 +47,7 @@ public class JsonLoginFilter extends GenericFilter {
         final HttpServletRequest req = (HttpServletRequest) request;
         final HttpServletResponse resp = (HttpServletResponse) response;
 
-        if (Constants.LOGIN_PATH.equals(req.getRequestURI()) && HttpMethod.POST.name().equalsIgnoreCase(req.getMethod())) {
+        if (Constants.LOGIN_PATH.equals(req.getServletPath()) && HttpMethod.POST.name().equalsIgnoreCase(req.getMethod())) {
 
         	try (InputStream inStream = req.getInputStream();
         		PrintWriter writer = resp.getWriter()) {
